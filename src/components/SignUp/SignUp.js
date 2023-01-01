43eecc68b2d85b6eb00fd02bb72dfe724a5daa89
Asha <asha.engrbd@gmail.com>
2023-01-02 02:09:19 +0600

    send email varification using react-firebase-hooks

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -4,13 +4,14 @@ import { Link, useNavigate } from 'react-router-dom';
 import google from '../../images/google-icon.png';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+// import { sendEmailVerification } from 'firebase/auth';
 
 const SignUp = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
-    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth);
+    const [createUserWithEmailAndPassword, user] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
 
     const navigate = useNavigate();
 
